7e4bd95a5157d99b3758d74a9cc0c9e4d325e3d5
Herve Pages <hpages@fhcrc.org>
2015-03-18 07:54:40 +0000

    remove definition of species() generic (now in BiocGenerics)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/UniProt.ws@100794 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -2,5 +2,4 @@ setGeneric("taxId", function(x) standardGeneric("taxId"))
 setGeneric("taxId<-", signature="x", function(x, value) standardGeneric("taxId<-"))
 ## taxIdUniprots is not intended to be exported.
 setGeneric("taxIdUniprots", function(x) standardGeneric("taxIdUniprots"))
-setGeneric("species", function(x) standardGeneric("species"))
 setGeneric("db", function(x) standardGeneric("db"))
