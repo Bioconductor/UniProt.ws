aa47ff036fd28bad3ab4ce4146ccd032cada22ad
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-04-25 14:19:29 -0400

    update manUniProt unit tests

--- inst/tinytest/test_mapUniProt.R
+++ inst/tinytest/test_mapUniProt.R
@@ -23,3 +23,35 @@ expect_identical(colnames(res), c("From", "Entry", "Entry.Name"))
 rf <- returnFields()
 expect_identical(names(rf), c("groupName", "label", "name"))
 expect_true(is.data.frame(rf))
+
+expect_error(
+    mapUniProt(
+        from = "Gene_Name",
+        to = "UniProtKB-Swiss-Prot",
+        columns = c("accession", "id"),
+        query = list(taxId = 9606)
+    ),
+    "'ids' must be a non-zero character vector"
+)
+
+expect_error(
+    mapUniProt(
+        from = "Gene_Name",
+        to = "UniProtKB-Swiss-Prot",
+        columns = c("accession", "id"),
+        query = list(taxId = 9606, ids = "")
+    ),
+    "'ids' must be a non-zero character vector"
+)
+
+keys <- list(taxId = 9606, ids = c("TP53", ""))
+## filters out nzchars
+res <- mapUniProt(
+    from = "Gene_Name",
+    to = "UniProtKB-Swiss-Prot",
+    columns = c("accession", "id"),
+    query = list(taxId = 9606, ids = c("TP53", ""))
+)
+expect_true(is.data.frame(res))
+expect_identical(nrow(res), 1L)
+expect_identical(colnames(res), c("From", "Entry", "Entry.Name"))
